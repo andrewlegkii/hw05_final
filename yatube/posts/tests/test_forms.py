f9b54db038063a9a9d20af6303a8546c3445fc52
Андрей Легкий <legkiy.andrey@bk.ru>
2022-10-06 23:27:43 +0300

    Исправления

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -66,8 +66,7 @@ class PostFormTests(TestCase):
         )
         self.assertEqual(Post.objects.count(), posts_count + 1)
         self.assertTrue(Post.objects.filter(text='Тестовый текст').exists())
-        self.assertFormError(uploaded, 'small.gif', 'image/gif',
-                             ('This field is the problem.'))
+        self.assertFormError(uploaded, 'small.gif', 'image/gif')
 
     def test_edit_post(self):
         old_post = self.post
