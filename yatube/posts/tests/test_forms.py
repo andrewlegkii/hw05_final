ee37959e96efcf349d0907976cc7aeeaebb04055
Андрей Легкий <legkiy.andrey@bk.ru>
2022-10-06 23:52:26 +0300

    Исправления

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -66,7 +66,7 @@ class PostFormTests(TestCase):
         )
         self.assertEqual(Post.objects.count(), posts_count + 1)
         self.assertTrue(Post.objects.filter(text='Тестовый текст').exists())
-        self.assertFormError()
+        self.assertFormError('form', 'field', 'errors')
 
     def test_edit_post(self):
         old_post = self.post
