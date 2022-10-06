93244f5d296e6b0d3c0ffa43c720b3847fe75853
Андрей Легкий <legkiy.andrey@bk.ru>
2022-10-06 23:09:16 +0300

    Исправления

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -58,10 +58,6 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    post_list = (
-        Post.objects.select_related("author", "group")
-        .filter(author=post.author).all()
-    )
     form = CommentForm(request.POST or None)
     comments = post.comments.all()
     context = {
@@ -97,6 +93,7 @@ def post_edit(request, post_id):
         return redirect('posts:post_detail', post_id)
     return render(request, 'posts/create_post.html',
                   {"form": form, 'post': post, })
+
 
 @login_required
 def add_comment(request, post_id):
@@ -138,7 +135,7 @@ def current_author(request, username):
     else:
         Follow.objects.create(user=user, author=author)
     return redirect('posts:profile', username=username)
-    
+
 
 @login_required
 def profile_unfollow(request, username):
